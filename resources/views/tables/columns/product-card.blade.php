ff98ec55cc4a06458a3b94730426eead958ac759
smallnews <1371606921@qq.com>
2025-02-05 17:12:26 +0800

    优化按需加载 product.css

--- resources/views/tables/columns/product-card.blade.php
+++ resources/views/tables/columns/product-card.blade.php
@@ -2,7 +2,11 @@
     $record = $getRecord();
 @endphp
 
-<div class="flex p-3 min-w-80 max-w-96" >
+<div 
+    class="flex p-3 min-w-80 max-w-96"
+    x-data="{}"
+    x-load-css="[@js(\Filament\Support\Facades\FilamentAsset::getStyleHref('product-styles', 'wsmallnews/product'))]"
+>
     <img class="size-24 rounded-lg" src="{{config('filesystems.disks.' . config('filament.default_filesystem_disk') . '.url') . '/' . $record->image}}" alt="">
 
     <div class="ml-3 flex-1 min-w-0">
