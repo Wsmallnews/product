610428e8fc4250012536b8972b4621019e12b04b
smallnews@qq.com <1371606921@qq.com>
2026-08-18 15:07:23 +0800

    优化产品表结构

--- src/Enums/ProductSpecType.php
+++ src/Enums/ProductSpecType.php
@@ -2,27 +2,32 @@
 
 namespace Wsmallnews\Product\Enums;
 
+use BackedEnum;
 use Filament\Support\Contracts\HasLabel;
 use Filament\Support\Contracts\HasIcon;
 use Filament\Support\Contracts\HasColor;
+use Filament\Support\Icons\Heroicon;
+use Illuminate\Contracts\Support\Htmlable;
 use Wsmallnews\Support\Enums\Traits\EnumHelper;
 
-Enum ProductSkuType :string implements HasLabel, HasIcon, HasColor
+Enum ProductSpecType :string implements HasLabel, HasIcon, HasColor
 {
-
     use EnumHelper;
 
     case Single = 'single';
 
     case Multiple = 'multiple';
 
+    case MainMultiple = 'main_multiple';
+
     case Unit = 'unit';
 
-    public function getLabel(): ?string
+    public function getLabel(): string | Htmlable | null
     {
         return match ($this) {
             self::Single => '单规格',
             self::Multiple => '多规格',
+            self::MainMultiple => '主多规格',
             self::Unit => '多单位',
         };
     }
@@ -33,17 +38,19 @@ Enum ProductSkuType :string implements HasLabel, HasIcon, HasColor
         return match ($this) {
             self::Single => 'gray',
             self::Multiple => 'warning',
+            self::MainMultiple => 'info',
             self::Unit => 'success',
         };
     }
 
 
-    public function getIcon(): ?string
+    public function getIcon(): string | BackedEnum | Htmlable | null
     {
         return match ($this) {
-            self::Single => 'heroicon-m-pencil',
-            self::Multiple => 'heroicon-m-eye',
-            self::Unit => 'heroicon-m-check',
+            self::Single => Heroicon::Bars2,
+            self::Multiple => Heroicon::Bars3,
+            self::MainMultiple => Heroicon::Bars4,
+            self::Unit => Heroicon::Bars3BottomLeft,
         };
     }
 }
